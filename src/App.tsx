358d97913d1e8e9cc983431bdf0a6c2bfec66b61
Dcadv789 <dc.advisors23@gmail.com>
2025-05-29 10:15:16 -0300

    Commit from Bolt to GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,26 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={
+        <Route element={
           <ProtectedRoute>
             <Layout>
               <Routes>
-                <Route path="/profile\" element={<Profile />} />
-                <Route path="/cadastros" element={<Cadastros />} />
-                <Route path="/" element={
+                <Route index element={
                   <div className="flex items-center justify-center h-full">
                     <p className="text-gray-400">Conteúdo será adicionado conforme suas instruções</p>
                   </div>
                 } />
+                <Route path="profile" element={<Profile />} />
+                <Route path="cadastros" element={<Cadastros />} />
               </Routes>
             </Layout>
           </ProtectedRoute>
-        } />
-        <Route path="*" element={<Navigate to="/\" replace />} />
+        }>
+          <Route path="/" />
+          <Route path="/profile" />
+          <Route path="/cadastros" />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
